4a87051c18b93723e4f3335b34c4b05c63e7f517
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-01-26 16:51:13 +0000

    Create 025_Path Sum.cs

---
+++ C#/Easy/025_Path Sum.cs
@@ -0,0 +1,67 @@
+/*Challenge link:https://leetcode.com/problems/path-sum/description/
+Question:
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+//see image in link
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+Example 2:
+
+
+//see image in link
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+*/
+
+//***************Solution********************
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        //check leaf node child
+        if(root.left == null && root.right == null)
+        return  targetSum == root.val;
+
+        //reduce targetSum each iteration
+        bool leftSum = HasPathSum(root.left, targetSum - root.val);
+        bool rightSum = HasPathSum(root.right, targetSum - root.val);
+        return leftSum || rightSum;
+    }
+}
