354effe496659807ff40b67b5e947da33dcbb5cc
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-07-11 19:15:07 +0100

    Create 067_Excel Sheet Column Number.cs

---
+++ C#/Easy/067_Excel Sheet Column Number.cs
@@ -0,0 +1,50 @@
+/*Challenge link:https://leetcode.com/problems/excel-sheet-column-number/description/
+Question:
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+ 
+
+Constraints:
+
+1 <= columnTitle.length <= 7
+columnTitle consists only of uppercase English letters.
+columnTitle is in the range ["A", "FXSHRXW"].
+*/
+
+//***************Solution********************
+public class Solution {
+    public int TitleToNumber(string columnTitle) {
+        int output=0;
+
+        for (int i=0;i<columnTitle.Length;i++){
+            int curr=columnTitle[i]-'A'+1;
+            output=output*26+curr;
+        }
+        
+        return output;
+    }
+}
