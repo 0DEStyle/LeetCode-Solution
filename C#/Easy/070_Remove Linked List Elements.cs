f3650d532d9c93381583cf5e9eebc9a6810c6f6f
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-07-13 19:37:02 +0100

    Create 070_Remove Linked List Elements.cs

---
+++ C#/Easy/070_Remove Linked List Elements.cs
@@ -0,0 +1,54 @@
+/*Challenge link:https://leetcode.com/problems/remove-linked-list-elements/description/
+Question:
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+*/
+
+//***************Solution********************
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+        ListNode  temp = new ListNode(0), curr = temp;
+        temp.next = head;
+        
+        while (curr.next != null){
+            if (curr.next.val == val)
+                curr.next = curr.next.next;
+            else
+                curr = curr.next;
+        }
+        return temp.next;
+    }
+}
