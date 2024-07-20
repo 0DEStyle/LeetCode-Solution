2396d395d1de2d9c447fd29ee174e14bbdbc536f
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-07-20 10:47:00 +0100

    Create 083_Add Digits.cs

---
+++ C#/Easy/083_Add Digits.cs
@@ -0,0 +1,39 @@
+/*Challenge link:https://leetcode.com/problems/add-digits/description/
+Question:
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+ 
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+*/
+
+//***************Solution********************
+public class Solution {
+    public int AddDigits(int num) {
+        //if num is 0, return 0
+        if(num == 0)return 0;
+        //mod 9 of num set to ans
+        int ans = num % 9;
+        //if ans is 0, return 9, else and
+        return ans == 0 ? 9 : ans;
+    }
+}
