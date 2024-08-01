4f2a335af63c8062da46d42e5f1fddc3d210addb
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-08-01 11:02:58 +0100

    Create 114_ Relative Ranks.cs

---
+++ C#/Easy/114_ Relative Ranks.cs
@@ -0,0 +1,62 @@
+/*Challenge link:https://leetcode.com/problems/relative-ranks/description/
+Question:
+You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+The 1st place athlete's rank is "Gold Medal".
+The 2nd place athlete's rank is "Silver Medal".
+The 3rd place athlete's rank is "Bronze Medal".
+For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+ 
+
+Example 1:
+
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+Example 2:
+
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+ 
+
+Constraints:
+
+n == score.length
+1 <= n <= 104
+0 <= score[i] <= 106
+All the values in score are unique.
+*/
+
+//***************Solution********************
+public class Solution {
+    public string[] FindRelativeRanks(int[] score) {
+        //dictionary to store current score and rank
+        var scoreDict = new Dictionary<int,int>();
+
+        //assign rank into dictionary
+        for(int i = 0; i<score.Length;i++)
+            scoreDict[score[i]] = i;
+        //sort score array in ascending order
+        Array.Sort(score,(x,y)=>y.CompareTo(x));
+        string[] result = new string[score.Length];
+
+        //assign rank based on the position after the sorting
+        for(int i = 0;i<score.Length;i++){
+            if(i == 0)
+                result[scoreDict[score[i]]] = "Gold Medal";
+            else if(i == 1)
+                result[scoreDict[score[i]]] = "Silver Medal";
+            else if(i == 2)
+                result[scoreDict[score[i]]] = "Bronze Medal";
+            else
+                result[scoreDict[score[i]]] = (i+1).ToString();
+        }
+        return result;
+    }
+}
