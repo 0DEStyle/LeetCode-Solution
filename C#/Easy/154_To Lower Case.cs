cbab440e8b2878ec1b80af75feab793abf6e2b87
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-08-10 21:50:34 +0100

    Create 154_To Lower Case.cs

---
+++ C#/Easy/154_To Lower Case.cs
@@ -0,0 +1,32 @@
+/*Challenge link:https://leetcode.com/problems/to-lower-case/description/
+Question:
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
+
+//***************Solution********************
+
+//Then simiplfied into one line by using an Lambda expression with Enumerable methods.
+public class Solution {
+    public string ToLowerCase(string s) => s.ToLower();
+}
