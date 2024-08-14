b0dd12903f6f3b13b9b2a633e6c46bf5d7bd0c92
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-08-14 22:32:47 +0100

    Create 174_Goat Latin.cs

---
+++ C#/Easy/174_Goat Latin.cs
@@ -0,0 +1,44 @@
+/*Challenge link:https://leetcode.com/problems/goat-latin/description/
+Question:
+You are given a string sentence that consist of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.) The rules of Goat Latin are as follows:
+
+If a word begins with a vowel ('a', 'e', 'i', 'o', or 'u'), append "ma" to the end of the word.
+For example, the word "apple" becomes "applema".
+If a word begins with a consonant (i.e., not a vowel), remove the first letter and append it to the end, then add "ma".
+For example, the word "goat" becomes "oatgma".
+Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+For example, the first word gets "a" added to the end, the second word gets "aa" added to the end, and so on.
+Return the final sentence representing the conversion from sentence to Goat Latin.
+
+ 
+
+Example 1:
+
+Input: sentence = "I speak Goat Latin"
+Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+Example 2:
+
+Input: sentence = "The quick brown fox jumped over the lazy dog"
+Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ 
+
+Constraints:
+
+1 <= sentence.length <= 150
+sentence consists of English letters and spaces.
+sentence has no leading or trailing spaces.
+All the words in sentence are separated by a single space.
+*/
+
+//***************Solution********************public class Solution {
+    public string ToGoatLatin(string s) 
+        => string.Join(' ', s.Split(' ').Select(goat));
+
+    private static string goat(string word, int index)
+        => char.ToLower(word[0]) is not ('a' or 'e' or 'i' or 'o' or 'u')
+            ? word[1..] + word[0] + "ma" + new string('a', index + 1)
+            : word + "ma" + new string('a', index + 1);
+
+}
