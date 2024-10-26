e24ebb7b1a127c15a0ed5ab4c4810a1f454cbdb1
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-10-26 10:42:50 +0100

    Create 257_Subtract the Product and Sum of Digits of an Integer.cs

---
+++ C#/Easy/257_Subtract the Product and Sum of Digits of an Integer.cs
@@ -0,0 +1,42 @@
+/*Challenge link:https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
+Question:
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+*/
+
+//***************Solution********************
+public class Solution {
+    public int SubtractProductAndSum(int n) {
+        int p = 1, s = 0;
+
+        while(n != 0){
+            p *= n % 10;  //product
+            s += n % 10;  //sum of its digits
+            n /= 10;
+        }
+
+        return p - s;
+    }
+}
