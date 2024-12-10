dd084ca449dd59b8b211dc67b085789311a010f1
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2024-12-10 00:53:29 +0000

    Create 302_Make The String Great.cs

---
+++ C#/Easy/302_Make The String Great.cs
@@ -0,0 +1,58 @@
+/*Challenge link:https://leetcode.com/problems/make-the-string-great/description/
+Question:
+Given a string s of lower and upper case English letters.
+
+A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+0 <= i <= s.length - 2
+s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+Notice that an empty string is also good.
+
+ 
+
+Example 1:
+
+Input: s = "leEeetcode"
+Output: "leetcode"
+Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+Example 2:
+
+Input: s = "abBAcC"
+Output: ""
+Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+"abBAcC" --> "aAcC" --> "cC" --> ""
+"abBAcC" --> "abBA" --> "aA" --> ""
+Example 3:
+
+Input: s = "s"
+Output: "s"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s contains only lower and upper case English letters.
+*/
+
+//***************Solution********************
+public class Solution {
+    public string MakeGood(string s) {
+        var sb = new StringBuilder();
+        
+        //loop through each charactter in s
+        //if string builder is greater than 0 and
+        // last element of string bulder - current character is equalt to space(ascii 32). reduce sb length by 1
+        //else append c to string builder.
+        foreach(var c in s)
+            if(sb.Length > 0 && Math.Abs(sb[^1] - c) == 32)
+                sb.Length--;
+            else
+                sb.Append(c);
+        return sb.ToString();
+    }
+}
+
