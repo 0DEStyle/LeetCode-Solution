5a974dcbb3af151a97d2f15e5660283f73e5dd70
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2025-01-09 10:57:57 +0000

    Create 332_Decode XORed Array.cs

---
+++ C#/Easy/332_Decode XORed Array.cs
@@ -0,0 +1,64 @@
+/*Challenge link:https://leetcode.com/problems/decode-xored-array/description/
+Question:
+There is a hidden integer array arr that consists of n non-negative integers.
+
+It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+
+You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+
+Return the original array arr. It can be proved that the answer exists and is unique.
+
+ 
+
+Example 1:
+
+Input: encoded = [1,2,3], first = 1
+Output: [1,0,2,1]
+Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+Example 2:
+
+Input: encoded = [6,2,7,3], first = 4
+Output: [4,2,0,7,4]
+ 
+
+Constraints:
+
+2 <= n <= 104
+encoded.length == n - 1
+0 <= encoded[i] <= 105
+0 <= first <= 105
+*/
+
+//***************Solution********************
+/*
+encoded[i] = arr[i] XOR arr[i + 1]
+
+ 2 => 0010
+ 1 => 0001
+res = 0011 ( 3 )
+
+if   a ^ b = c
+then     a = c ^ b
+
+
+[1,2,3]
+1 x 0 > 1
+0 x 2 > 2
+2 x 1 > 3
+
+[first, ...]
+
+*/
+public class Solution {
+    public int[] Decode(int[] encoded, int first) {
+        var res = new int[encoded.Length + 1];
+        //set first
+        res[0] = first;
+
+        //a = c ^ b
+        for(int i = 0; i < res.Length - 1; i++)
+            res[i + 1] = res[i] ^ encoded[i];
+
+        return res;
+    }
+}
