0cb3a2f49ab9d45311c5781f751e6c9f08f82d2d
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2025-01-11 14:33:21 +0000

    Create 334_Maximum Number of Balls in a Box.cs

---
+++ C#/Easy/334_Maximum Number of Balls in a Box.cs
@@ -0,0 +1,74 @@
+/*Challenge link:https://leetcode.com/problems/maximum-number-of-balls-in-a-box/description/
+Question:
+You are working in a ball factory where you have n balls numbered from lowLimit up to highLimit inclusive (i.e., n == highLimit - lowLimit + 1), and an infinite number of boxes numbered from 1 to infinity.
+
+Your job at this factory is to put each ball in the box with a number equal to the sum of digits of the ball's number. For example, the ball number 321 will be put in the box number 3 + 2 + 1 = 6 and the ball number 10 will be put in the box number 1 + 0 = 1.
+
+Given two integers lowLimit and highLimit, return the number of balls in the box with the most balls.
+
+ 
+
+Example 1:
+
+Input: lowLimit = 1, highLimit = 10
+Output: 2
+Explanation:
+Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+Ball Count:  2 1 1 1 1 1 1 1 1 0  0  ...
+Box 1 has the most number of balls with 2 balls.
+Example 2:
+
+Input: lowLimit = 5, highLimit = 15
+Output: 2
+Explanation:
+Box Number:  1 2 3 4 5 6 7 8 9 10 11 ...
+Ball Count:  1 1 1 1 2 2 1 1 1 0  0  ...
+Boxes 5 and 6 have the most number of balls with 2 balls in each.
+Example 3:
+
+Input: lowLimit = 19, highLimit = 28
+Output: 2
+Explanation:
+Box Number:  1 2 3 4 5 6 7 8 9 10 11 12 ...
+Ball Count:  0 1 1 1 1 1 1 1 1 2  0  0  ...
+Box 10 has the most number of balls with 2 balls.
+ 
+
+Constraints:
+
+1 <= lowLimit <= highLimit <= 105
+*/
+
+//***************Solution********************
+/*
+n == highLimit - lowLimit + 1)
+n == 10 - 1 + 1 
+
+put each ball in the box 
+with a number equal to the sum of 
+digits of the ball's number.
+
+1 - 10
+Box Number:  1 2 3 4 5 6 7 8 9 10  ...
+Ball Count:  2 1 1 1 1 1 1 1 1 0   ...
+sum          1 2 3 4 5 6 7 8 9 1  -> 1 appeared 2 times
+*/
+public class Solution {
+    public int CountBalls(int low, int high) {
+        var ans = new Dictionary<int,int>();
+
+        //loop from low to high limit
+        for(int i = low;i <= high; i++){
+            //convert i to string, offset ascii value and get sum of each digit
+            var n = i.ToString().Select(x => x - '0').Sum();
+
+            //accumulate count.
+            if(ans.ContainsKey(n))
+                ans[n]++;
+            else 
+                ans[n] = 1;
+        }
+        //return the element with max count.
+        return ans.Values.Max();
+    }
+}
