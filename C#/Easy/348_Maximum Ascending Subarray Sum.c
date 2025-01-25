06b795752515f7418eb25727bff0a6f945e9d9db
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2025-01-25 14:35:58 +0000

    Create 348_Maximum Ascending Subarray Sum.c

---
+++ C#/Easy/348_Maximum Ascending Subarray Sum.c
@@ -0,0 +1,55 @@
+/*Challenge link:https://leetcode.com/problems/maximum-ascending-subarray-sum/description/
+Question:
+Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+
+A subarray is defined as a contiguous sequence of numbers in an array.
+
+A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of size 1 is ascending.
+
+ 
+
+Example 1:
+
+Input: nums = [10,20,30,5,10,50]
+Output: 65
+Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
+Example 2:
+
+Input: nums = [10,20,30,40,50]
+Output: 150
+Explanation: [10,20,30,40,50] is the ascending subarray with the maximum sum of 150.
+Example 3:
+
+Input: nums = [12,17,15,13,10,11,12]
+Output: 33
+Explanation: [10,11,12] is the ascending subarray with the maximum sum of 33.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+*/
+
+//***************Solution********************
+public class Solution {
+    public int MaxAscendingSum(int[] nums) {
+        int start=0, n = nums.Length;
+        int maxSum = nums[0], currSum = nums[0];
+
+        for(int end = 0;end < n; end++){
+            if(end == 0)
+                continue;
+            
+            //if end is less than or equal to previous, set start to end, reset sum
+            if(nums[end] <= nums[end - 1]){
+                start = end;
+                currSum = 0;
+            }
+            //accumulate sum for end, select max between max and current sum
+            currSum += nums[end];
+            maxSum = Math.Max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+}
