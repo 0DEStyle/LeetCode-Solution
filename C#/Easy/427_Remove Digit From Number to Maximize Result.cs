9ac03810dc86a9bfb22c5aac65701ab3cb19d281
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2025-04-14 11:05:44 +0100

    Create 427_Remove Digit From Number to Maximize Result.cs

---
+++ C#/Easy/427_Remove Digit From Number to Maximize Result.cs
@@ -0,0 +1,51 @@
+/*Challenge link:https://leetcode.com/problems/remove-digit-from-number-to-maximize-result/description/
+Question:
+You are given a string number representing a positive integer and a character digit.
+
+Return the resulting string after removing exactly one occurrence of digit from number such that the value of the resulting string in decimal form is maximized. The test cases are generated such that digit occurs at least once in number.
+
+ 
+
+Example 1:
+
+Input: number = "123", digit = "3"
+Output: "12"
+Explanation: There is only one '3' in "123". After removing '3', the result is "12".
+Example 2:
+
+Input: number = "1231", digit = "1"
+Output: "231"
+Explanation: We can remove the first '1' to get "231" or remove the second '1' to get "123".
+Since 231 > 123, we return "231".
+Example 3:
+
+Input: number = "551", digit = "5"
+Output: "51"
+Explanation: We can remove either the first or second '5' from "551".
+Both result in the string "51".
+ 
+
+Constraints:
+
+2 <= number.length <= 100
+number consists of digits from '1' to '9'.
+digit is a digit from '1' to '9'.
+digit occurs at least once in number.
+*/
+
+//***************Solution********************
+public class Solution {
+    public string RemoveDigit(string n, char digit) {
+        var nums = new List<string>();
+
+        //lopp through each number
+        //if current digit in n equal to digit
+        //remove n at i by 1, add to nums
+        for(int i = 0; i < n.Length; i++)
+            if(n[i] == digit)
+                nums.Add(n.Remove(i, 1));
+
+        //return max value from nums list
+        return nums.Max();
+    }
+}
